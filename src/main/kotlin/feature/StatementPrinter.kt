6113f97e1df400dfcd8d1588275b52462f7aac11
Alejandro <alejandro.miralles@wallapop.com>
2021-07-12 11:14:39 +0200

    Finished StatementPrinter unit test and functionality

--- src/main/kotlin/feature/StatementPrinter.kt
+++ src/main/kotlin/feature/StatementPrinter.kt
@@ -1,10 +1,34 @@
 package feature
 
-class StatementPrinter(val console: Console) {
+import java.text.DecimalFormat
+import java.util.Collections
+import java.util.concurrent.atomic.AtomicInteger
+import javax.swing.text.NumberFormatter
+
+class StatementPrinter(private val console: Console) {
+
     private val statementHeader = "DATE | AMOUNT | BALANCE"
+    private val decimalFormatter = DecimalFormat("#.00")
 
     fun print(transactions: List<Transaction>) {
         console.printLine(statementHeader)
+        printStatementLines(transactions)
+    }
+
+    private fun printStatementLines(transactions: List<Transaction>) {
+        val runningBalance = AtomicInteger(0)
+        transactions.map { transaction -> statementLine(transaction, runningBalance)
+        }.sortedDescending().forEach { statementLine -> printStatement(statementLine) }
+    }
+
+    private fun statementLine(transaction: Transaction, runningBalance: AtomicInteger): String {
+        return transaction.date!! +
+                " | ${decimalFormatter.format(transaction.amount)}" +
+                " | ${decimalFormatter.format(runningBalance.addAndGet(transaction.amount))}"
+    }
+
+    private fun printStatement(statementLine: String){
+        console.printLine(statementLine)
     }
 
 }
--- src/test/kotlin/feature/StatementPrinterShould.kt
+++ src/test/kotlin/feature/StatementPrinterShould.kt
@@ -1,26 +1,49 @@
 package feature
 
+import kotlin.test.BeforeTest
 import kotlin.test.Test
+import org.mockito.Mockito.inOrder
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
 
-
-internal class StatementPrinterShould{
+internal class StatementPrinterShould {
 
     private val noTransactions: List<Transaction> = emptyList()
     private val console = mock(Console::class.java)
+    private lateinit var statementPrinter: StatementPrinter
+
+    @BeforeTest
+    fun initialize() {
+        statementPrinter = StatementPrinter(console)
+    }
 
     @Test
-    fun `always print the header`(){
-
-        val statementPrinter = StatementPrinter(console)
+    fun `always print the header`() {
         statementPrinter.print(noTransactions)
 
         verify(console).printLine("DATE | AMOUNT | BALANCE")
     }
 
     @Test
-    fun `print transactions in chronological order`(){
+    fun `print transactions in reverse chronological order`() {
+        val transactions = listOfTransactionsContaining(
+            deposit("01/06/2021", 1000),
+            withdrawal("02/06/2021", 100),
+            deposit("10/06/2021", 500)
+        )
 
+        statementPrinter.print(transactions)
+
+        val inOrder = inOrder(console)
+        inOrder.verify(console).printLine("DATE | AMOUNT | BALANCE")
+        inOrder.verify(console).printLine("10/06/2021 | 500.00 | 1400.00")
+        inOrder.verify(console).printLine("02/06/2021 | -100.00 | 900.00")
+        inOrder.verify(console).printLine("01/06/2021 | 1000.00 | 1000.00")
     }
+
+    private fun listOfTransactionsContaining(vararg transactions: Transaction) = transactions.toList()
+
+    private fun withdrawal(date: String, amount: Int) = Transaction(date, -amount)
+
+    private fun deposit(date: String, amount: Int) = Transaction(date, amount)
 }
